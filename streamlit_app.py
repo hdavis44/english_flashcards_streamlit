f47c670bf4ed1763933edda4030efa091812ca5b
Henry Davis <henryo.davis@gmail.com>
2024-04-18 16:35:41 +0200

    added 'my translation' functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,15 @@ def callback2():
 def callback3():
     state.button3_clicked = True
 
-# def callbacktext():
-#     state.user_translation = user_translation
+def callback4():
+    state.button4_clicked = True
+
+def callback5():
+    state.button5_clicked = True
+
+def callback_text():
+    state.user_translation = state.user_input
+    state.user_input = ""
 
 # ---------- session state ----------
 
@@ -32,6 +39,12 @@ if "button2_clicked" not in state:
 
 if "button3_clicked" not in state:
     state.button3_clicked = False
+
+if "button4_clicked" not in state:
+    state.button4_clicked = False
+
+if "button5_clicked" not in state:
+    state.button5_clicked = False
 
 if "df" not in state:
     state.df = None
@@ -46,13 +59,13 @@ if "current_row" not in state:
     state.current_row = None
 
 if "user_translation" not in state:
-    state.user_translation = None
+    state.user_translation = ""
 
 # ---------- main page ----------
 
-st.markdown("# English Homework Helper")
+st.markdown("## English Homework Helper")
 
-uploaded_file = st.file_uploader("Upload a .csv homework file", type=["csv"])
+uploaded_file = st.file_uploader("Upload a .csv homework file", type=["csv"], label_visibility="collapsed")
 
 if uploaded_file is not None:
 
@@ -69,11 +82,11 @@ if uploaded_file is not None:
         state.question_pool = state.df_remaining.sample(sample_size, replace=False)
         state.df_remaining = state.df_remaining[~state.df_remaining.index.isin(state.question_pool.index.to_list())]
 
-    st.write(
-        f"There are {state.df.shape[0]} phrases in this file. Selecting {state.question_pool.shape[0]} random phrases to practice with."
-    )
+    # st.write(
+    #     f"There are {state.df.shape[0]} phrases in this file. Selecting {state.question_pool.shape[0]} random phrases to practice with."
+    # )
 
-    st.divider()
+    # st.divider()
 
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -93,8 +106,9 @@ if uploaded_file is not None:
         user_translation = st.text_input(
             "translation",
             label_visibility="collapsed",
-            key="user_translation",
-            placeholder="my translation"
+            key="user_input",
+            placeholder="my translation",
+            on_change=callback_text
         )
 
     if phrase:
@@ -118,11 +132,23 @@ if uploaded_file is not None:
             with col2:
                 st.markdown(state.current_hint)
 
-    elif translation or user_translation:
+    elif translation or state.user_translation:
         if state.button1_clicked:
             with col1:
                 st.markdown(state.current_phrase)
             with col2:
                 st.markdown(state.current_hint)
             with col3:
-                st.markdown(state.current_translation)
+                st.markdown(f"Your answer: {state.user_translation}")
+                st.markdown(f"Correct answer: {state.current_translation}")
+
+            col4, col5 = st.columns(2)
+            with col4:
+                correct = st.button(":heavy_check_mark:", on_click=callback4, use_container_width=True)
+            with col5:
+                incorrect = st.button(":x:", on_click=callback5, use_container_width=True)
+
+            if correct:
+                pass
+            elif incorrect:
+                pass

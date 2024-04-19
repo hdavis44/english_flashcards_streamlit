78e0baf64d2067f73124062888d0354c5837fb22
Henry Davis <henryo.davis@gmail.com>
2024-04-19 14:58:49 +0200

    added full functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ def callback5():
 
 def callback_text():
     state.user_translation = state.user_input
-    state.user_input = ""
+    state.user_input = None
 
 # ---------- session state ----------
 
@@ -49,17 +49,25 @@ if "button5_clicked" not in state:
 if "df" not in state:
     state.df = None
 
-if "question_pool" not in state:
-    state.question_pool = None
-
 if "df_remaining" not in state:
     state.df_remaining = None
+
+if "df_question_pool" not in state:
+    state.df_question_pool = None
+
+if "df_complete" not in state:
+    state.df_complete = None
+
+if "df_review" not in state:
+    state.df_review = None
 
 if "current_row" not in state:
     state.current_row = None
 
 if "user_translation" not in state:
-    state.user_translation = ""
+    state.user_translation = None
+
+SAMPLE_SIZE = 20
 
 # ---------- main page ----------
 
@@ -69,24 +77,23 @@ uploaded_file = st.file_uploader("Upload a .csv homework file", type=["csv"], la
 
 if uploaded_file is not None:
 
+    st.divider()
+
     if state.df is None:
         state.df = pd.read_csv(uploaded_file, delimiter=";", usecols=["phrase","hint","translation"])
 
     if state.df_remaining is None:
         state.df_remaining = state.df.copy()
 
-    if state.question_pool is None:
-        sample_size = 20
-        if state.df_remaining.shape[0] < 20:
-            sample_size = state.df_remaining.shape[0]
-        state.question_pool = state.df_remaining.sample(sample_size, replace=False)
-        state.df_remaining = state.df_remaining[~state.df_remaining.index.isin(state.question_pool.index.to_list())]
+    if state.df_question_pool is None:
+        state.df_question_pool = state.df_remaining.sample(SAMPLE_SIZE, replace=False)
+        state.df_remaining = state.df_remaining[~state.df_remaining.index.isin(state.df_question_pool.index.to_list())]
 
-    # st.write(
-    #     f"There are {state.df.shape[0]} phrases in this file. Selecting {state.question_pool.shape[0]} random phrases to practice with."
-    # )
+    if state.df_complete is None:
+        state.df_complete = pd.DataFrame({"phrase":[], "hint":[], "translation":[]})
 
-    # st.divider()
+    if state.df_review is None:
+        state.df_review = pd.DataFrame({"phrase":[], "hint":[], "translation":[]})
 
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -112,24 +119,16 @@ if uploaded_file is not None:
         )
 
     if phrase:
-
         state.user_translation = None
-        # state.button2_clicked = False
-        # state.button3_clicked = False
-
-        if state.question_pool is None:
-            state.question_pool = state.df.sample(20)
-
-        state.current_row = state.question_pool.sample()
+        state.current_row = state.df_question_pool.sample()
         state.current_index = state.current_row.index
         state.current_phrase = state.current_row["phrase"].item()
         state.current_hint = state.current_row["hint"].item()
         state.current_translation = state.current_row["translation"].item()
-
         with col1:
             st.markdown(state.current_phrase)
 
-    elif hint:
+    if hint:
         if state.button1_clicked:
             with col1:
                 st.markdown(state.current_phrase)
@@ -146,13 +145,54 @@ if uploaded_file is not None:
                 st.markdown(f"Your answer: {state.user_translation}")
                 st.markdown(f"Correct answer: {state.current_translation}")
 
-            col4, col5 = st.columns(2)
-            with col4:
-                correct = st.button(":heavy_check_mark:", on_click=callback4, use_container_width=True)
-            with col5:
-                incorrect = st.button(":x:", on_click=callback5, use_container_width=True)
+    col4, col5 = st.columns(2)
+    with col4:
+        correct = st.button(":heavy_check_mark:", on_click=callback4, use_container_width=True)
+    with col5:
+        incorrect = st.button(":x:", on_click=callback5, use_container_width=True)
 
-            if correct:
-                pass
-            elif incorrect:
-                pass
+    if correct:
+        state.df_complete.loc[state.current_index[0]] = state.current_row.loc[state.current_index[0]]
+        state.df_question_pool.drop(index=state.current_index, inplace=True)
+        if state.df_question_pool.shape[0] == 0:
+            state.df_question_pool = state.df_review.copy()
+            state.df_review = pd.DataFrame({"phrase":[], "hint":[], "translation":[]})
+            if state.df_question_pool.shape[0] == 0:
+                if state.df_remaining.shape[0] < SAMPLE_SIZE:
+                    state.df_question_pool = state.df_remaining.copy()
+                else:
+                    state.df_question_pool = state.df_remaining.sample(SAMPLE_SIZE)
+                state.df_remaining = state.df_remaining[~state.df_remaining.index.isin(state.df_question_pool.index.to_list())]
+
+    elif incorrect:
+        state.df_review.loc[state.current_index[0]] = state.current_row.loc[state.current_index[0]]
+        state.df_question_pool.drop(index=state.current_index, inplace=True)
+
+        if state.df_question_pool.shape[0] == 0:
+            state.df_question_pool = state.df_review.copy()
+            state.df_review = pd.DataFrame({"phrase":[], "hint":[], "translation":[]})
+            if state.df_question_pool.shape[0] == 0:
+                if state.df_remaining.shape[0] < SAMPLE_SIZE:
+                    state.df_question_pool = state.df_remaining.copy()
+                    if state.df_question_pool.shape[0] == 0:
+                        st.empty()
+                else:
+                    state.df_question_pool = state.df_remaining.sample(SAMPLE_SIZE)
+                state.df_remaining = state.df_remaining[~state.df_remaining.index.isin(state.df_question_pool.index.to_list())]
+
+# ---------- TESTING ----------
+
+    # if state.button1_clicked:
+    #     col6, col7, col8, col9 = st.columns(4)
+    #     with col6:
+    #         st.write(f"state.current_phrase")
+    #         st.write(state.current_phrase)
+    #     with col7:
+    #         st.write(f"state.df_question_pool:")
+    #         st.write(state.df_question_pool["phrase"])
+    #     with col8:
+    #         st.write(f"state.df_complete:")
+    #         st.write(state.df_complete["phrase"])
+    #     with col9:
+    #         st.write(f"state.df_review:")
+    #         st.write(state.df_review["phrase"])
